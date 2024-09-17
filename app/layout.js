dc708da8c485c14aa229c82cc66bd2ada4a2417c
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-09-17 11:42:24 -0300

    main layout margins

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.className}  antialiased`}
+        className={`${inter.className} antialiased flex justify-center`}
       >
         <ThemeProvider
           attribute="class"
@@ -26,7 +26,9 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <div className='sm:mx-[10%] md:mx-[15%] mx-[8px] max-w-[1200px] w-full'>
+            {children}
+          </div>
         </ThemeProvider>
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -3,10 +3,12 @@ import { ModeToggle } from "./components/modeToggle";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex flex-col items-center justify-items-center min-h-screen gap-16 ">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
         <ModeToggle></ModeToggle>
-        klnblkjhlkhlk
+        <div className="w-full text-orange-600 bg-black">
+          lkfdjsñlkjfdña
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <p>Footer</p>
