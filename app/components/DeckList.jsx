726f053a86b39fa3d97be16700eb57495a7499b9
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-09-19 13:19:10 -0300

    starting deck list entry component

--- app/components/DeckList.jsx
+++ app/components/DeckList.jsx
@@ -13,14 +13,17 @@ function DeckListEntry({ props }){
     return (
         <div className="flex justify-between items-center p-4 gap-16">
             {/* General Container */}
-            <div className="flex flex-col gap-3">
+            <div className="max-w-[450px] flex flex-col gap-3">
                 <p className="font-medium text-base text-gray-200">
                     🌐 Deck Title
                 </p>
                 <p className="font-light text-sm text-gray-400">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui dolore ipsa ipsam assumenda tempore quisquam ratione perferendis odio, quibusdam incidunt repellendus vero vel laudantium, nemo asperiores delectus obcaecati esse! Facere!
                 </p>
-                <Badge variant="outline" className='w-fit text-xxs font-normal'>Tag</Badge>
+                <div className="flex gap-2">
+                    <Badge variant="outline" className='w-fit text-xxs font-normal'>Tag</Badge>
+                    <Badge variant="outline" className='w-fit text-xxs font-normal'>Tag</Badge>
+                </div>
             </div>
             <div>
                 Deck buttons
