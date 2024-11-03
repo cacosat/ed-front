e66a7f41ff7eeef89641dd1cd7a1fd9305d04db5
cacosat <86966872+cacosat@users.noreply.github.com>
2024-11-03 13:40:24 -0300

    auth provider

--- app/contexts/AuthContext.js
+++ app/contexts/AuthContext.js
@@ -14,7 +14,27 @@ export function AuthProvider({ children }) {
 
     const login = async (email, password) => {
         try {
-            
+            setAccessToken(null);
+            setUser(null);
+
+            const response = await fetch(`${API_BASE_URL}/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include',
+                body: JSON.stringify({ email, password })
+            })
+
+            if (!response.ok) {
+                const error = await response.json();
+                throw new Error(`Login failed` || error.message);
+            }
+
+            const data = await response.json();
+            setAccessToken(data.accessToken);
+            setUser(data.user);
+
         } catch (error) {
             console.error('Login error: ', error);
             throw error;
@@ -23,7 +43,24 @@ export function AuthProvider({ children }) {
 
     const register = async (email, password) => {
         try {
+            const response = fetch(`${API_BASE_URL}/auth/register`, {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                body: JSON.stringify({ email, password })
+            })
+
+            if (!response.ok) {
+                const error = await response.json()
+                throw new Error("Registration error" || error.message)
+            }
             
+            const data = await response.json();
+            
+            setAccessToken(data.accessToken);
+            setUser(data.user);
         } catch (error) {
             console.error('Registration error: ', error);
             throw error;
@@ -32,7 +69,17 @@ export function AuthProvider({ children }) {
 
     const logout = async () => {
         try {
-            
+            const response = await fetch(`${API_BASE_URL}/auth/logout`, {
+                method: 'POST',
+                credentials: 'include'
+            })
+
+            if (!response.ok) {
+                throw new Error('Logout failed');
+            }
+
+            setAccessToken(null);
+            setUser(null);
         } catch (error) {
             console.error('Logout error: ', error);
             throw error;
